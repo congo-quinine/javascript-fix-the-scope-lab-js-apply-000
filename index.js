67628cd258917a921a8421d12b6b3c216deba2ad
Rob Parker <congo.quinine@gmail.com>
2018-07-02 13:18:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var animal = 'dog'
+var animal = 'cat'
 
 function myAnimal() {
   return animal
 }
+myAnimal();
 
 function yourAnimal() {
   // How can we make sure that this function
@@ -10,13 +11,16 @@ function yourAnimal() {
   // P.S.: You can't just hard-code 'cat' below
   return animal
 }
+yourAnimal();
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
+add2(570);
+
 
 var funkyFunction = function() {
   return function() {
@@ -26,4 +30,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction();
