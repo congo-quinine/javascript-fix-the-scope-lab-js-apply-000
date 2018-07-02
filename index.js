3c49e366e10b787a6acde04483b1b9cf6b1baa81
Rob Parker <congo.quinine@gmail.com>
2018-07-02 13:25:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,16 +5,13 @@ function myAnimal() {
 }
 myAnimal();
 
-function yourAnimal(str) {
-  if (str === 'dog'){
-    animal = 'cat';
-  }
+function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
 }
-yourAnimal(animal);
+yourAnimal();
 
 function add2(n) {
   const two = 2
